9e61873c227a76d4c068ac49d1ced85b2d4daf28
tuckwoods848-arch <tuck.woods848@students.cmcss.net>
2026-03-06 07:39:31 -0600

    Update task1.py

---
+++ task1.py
@@ -0,0 +1,30 @@
+torch_lit = True
+if torch_lit:
+   outcome = "Flicker: The light shines!"
+else:
+    outcome = "Doom: The light is out."
+print(outcome)
+"""
+           /|
+        /\/ |/\
+        \  ^   | /\  /\
+  (\/\  / ^   /\/  )/^ )
+   \  \/^ /\       ^  /
+    )^       ^ \     (
+   (   ^   ^      ^\  )
+    \___\/____/______/
+    [________________]
+     |              |
+     |     //\\     |
+     |    <<()>>    |
+     |     \\//     |
+      \____________/
+          |    |
+          |    |
+          |    |
+          |    |
+          |    |
+          |    |
+          |    |
+          
+  """
