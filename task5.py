d36ab250bb268661783fc46a7470247c425bfbbe
tuckwoods848-arch <tuck.woods848@students.cmcss.net>
2026-03-06 07:47:58 -0600

    Update task5.py

---
+++ task5.py
@@ -0,0 +1,9 @@
+
+from task2 import outcome
+escaped = True
+if escaped:
+    outcome = "Legend: you have escaped and rode a hourse to freedom."
+else:
+    outcome = "Doom: you were found by the Royal Gaurds."
+print(outcome)
+
